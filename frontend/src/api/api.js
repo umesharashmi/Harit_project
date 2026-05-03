ea6f4204c155c74be8b5fdfb18231d20382516c6
umesharashmi <umesharashmi2001@gmail.com>
2026-05-03 10:07:40 +0530

    frontend build fixed

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 //  JWT TOKEN HEADER FUNCTION
 const getAuthHeader = () => {
