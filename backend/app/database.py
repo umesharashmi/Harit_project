d98ba0a218958b3e30334278ad8c4d5795e14837
umesharashmi <umesharashmi2001@gmail.com>
2026-05-04 12:35:11 +0530

    change the database

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,22 +6,11 @@ from dotenv import load_dotenv
 # load .env
 load_dotenv()
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-#  check if DATABASE_URL exists, nathnam old logic use karanawa
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-if DATABASE_URL:
-    engine = create_engine(
-        DATABASE_URL,
-        connect_args={"check_same_thread": False}
-    )
-else:
-    db_path = os.path.join(BASE_DIR, "prices.db")
-    engine = create_engine(
-        f"sqlite:///{db_path}",
-        connect_args={"check_same_thread": False}
-    )
+# PostgreSQL engine
+engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
 Base = declarative_base()
