493e2234e059cf578ac876bc8c0cd3ec5547b6a0
umesharashmi <umesharashmi2001@gmail.com>
2026-05-07 16:15:49 +0530

    update stock_sraper.py

--- backend/services/stock_scraper.py
+++ backend/services/stock_scraper.py
@@ -3,31 +3,29 @@ import os
 
 URL = "https://www.cse.lk/publications/cse-daily"
 
-SAVE_DIR = "pdfs/stocks"
+# project root based folder
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+SAVE_DIR = os.path.join(BASE_DIR, "stocks")
 
 
 def download_latest_pdf():
 
+    # ✅ auto create folder if not exists
     os.makedirs(SAVE_DIR, exist_ok=True)
 
     with sync_playwright() as p:
 
         browser = p.chromium.launch(
             headless=True,
-            args=[
-                "--no-sandbox",
-                "--disable-dev-shm-usage"
-            ]
+            args=["--no-sandbox", "--disable-dev-shm-usage"]
         )
 
         page = browser.new_page()
 
         page.goto(URL)
-
         page.wait_for_load_state("networkidle")
 
         with page.expect_download() as download_info:
-
             page.locator("text=Download").first.click()
 
         download = download_info.value
