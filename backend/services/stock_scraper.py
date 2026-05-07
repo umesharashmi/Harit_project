a66f87df48051f54162dc2ca54a6e1134021abbf
umesharashmi <umesharashmi2001@gmail.com>
2026-05-07 16:38:46 +0530

    update stock_sraper.py

--- backend/services/stock_scraper.py
+++ backend/services/stock_scraper.py
@@ -4,15 +4,30 @@ import os
 URL = "https://www.cse.lk/publications/cse-daily"
 
 
+def clean_old_pdfs(folder):
+
+    if os.path.exists(folder):
+
+        for file in os.listdir(folder):
+
+            file_path = os.path.join(folder, file)
+
+            if os.path.isfile(file_path):
+
+                os.remove(file_path)
+
+                print("🗑️ DELETED OLD FILE:", file_path)
+
+
 def download_latest_pdf():
 
-    # 🔥 auto detect project root
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-    # 🔥 auto create folder inside project
     SAVE_DIR = os.path.join(BASE_DIR, "stocks")
 
     os.makedirs(SAVE_DIR, exist_ok=True)
+
+    # 🔥 STEP 1: delete old PDFs first
+    clean_old_pdfs(SAVE_DIR)
 
     with sync_playwright() as p:
 
@@ -33,13 +48,12 @@ def download_latest_pdf():
 
         filename = download.suggested_filename
 
-        # 🔥 auto safe file path
         filepath = os.path.join(SAVE_DIR, filename)
 
         download.save_as(filepath)
 
         browser.close()
 
-        print("✅ PDF DOWNLOADED:", filepath)
+        print("✅ NEW PDF DOWNLOADED:", filepath)
 
         return filepath
