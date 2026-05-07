6ef2ead6c124217c4117216102968860e7fa2f1c
umesharashmi <umesharashmi2001@gmail.com>
2026-05-07 12:04:41 +0530

    update stock file

--- backend/services/stock_scraper.py
+++ backend/services/stock_scraper.py
@@ -20,6 +20,8 @@ def download_latest_pdf():
     options = webdriver.ChromeOptions()
 
     options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install()),
