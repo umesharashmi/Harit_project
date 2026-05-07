767c58f58e4f5b3401c8afa23d3babebf0b69a7b
umesharashmi <umesharashmi2001@gmail.com>
2026-05-07 09:23:42 +0530

    update stock_scraper.py

--- backend/services/stock_scraper.py
+++ backend/services/stock_scraper.py
@@ -1,56 +1,62 @@
-from datetime import datetime, timedelta
 import requests
 import os
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
-BASE = "https://www.cse.lk/publications/cse-daily"
+BASE = "https://www.cse.lk"
+URL = BASE + "/publications/cse-daily.component.html"
+
 DIR = "stock_pdfs"
-
-def get_latest_pdfs():
-    pdfs = []
-    today = datetime.today()
-
-    for i in range(15):  # increase range
-        d = today - timedelta(days=i)
-
-        url = BASE + f"cse_daily_{d.strftime('%Y_%m_%d')}.pdf"
-
-        r = requests.get(url)
-
-        if r.status_code == 200:
-            pdfs.append(url)
-
-        if len(pdfs) == 3:
-            break
-
-    return pdfs
 
 
 def download_all():
     os.makedirs(DIR, exist_ok=True)
 
-    links = get_latest_pdfs()
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
 
-    print("FOUND PDFs:", links)
+    response = requests.get(URL, headers=headers)
 
-    files = []
+    print("STATUS:", response.status_code)
 
-    for url in links:
-        name = url.split("/")[-1]
-        path = f"{DIR}/{name}"
+    soup = BeautifulSoup(response.text, "html.parser")
 
-        if not os.path.exists(path):
-            print("⬇️ Downloading:", name)
+    pdfs = []
 
-            r = requests.get(url)
+    for a in soup.find_all("a", href=True):
 
-            if r.status_code != 200:
-                print("❌ Failed:", url)
-                continue
+        href = a["href"]
+
+        if ".pdf" in href.lower():
+
+            full = urljoin(BASE, href)
+
+            pdfs.append(full)
+
+    print("FOUND PDFs:", pdfs)
+
+    downloaded = []
+
+    for link in pdfs:
+
+        try:
+            filename = link.split("/")[-1]
+
+            path = os.path.join(DIR, filename)
+
+            r = requests.get(link, headers=headers)
 
             with open(path, "wb") as f:
                 f.write(r.content)
 
-        files.append({"file": path})
+            downloaded.append({
+                "file": path
+            })
 
-    print("FILES:", files)
-    return files
+            print("DOWNLOADED:", filename)
+
+        except Exception as e:
+            print("DOWNLOAD ERROR:", e)
+
+    return downloaded
